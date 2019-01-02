027bf0e74cdb6f826265aaff92f7d743f8ec8b92
MBoretto <marco.bore@gmail.com>
2019-01-02 21:41:28 +0000

    Fix code sniffer error

--- src/Entities/NavigationKeyboard.php
+++ src/Entities/NavigationKeyboard.php
@@ -16,15 +16,15 @@ class NavigationKeyboard extends InlineKeyboardMarkup
     {
         $buttons = [];
         if (!is_null($payload_left)) {
-            $buttons[] = new InlineKeyboardButton(['text' =>  '◁', 'callback_data' => $payload_left]);
+            $buttons[] = new InlineKeyboardButton(['text' => '◁', 'callback_data' => $payload_left]);
         }
 
         if (!is_null($payload_menu)) {
-            $buttons[] = new InlineKeyboardButton(['text' =>  $symbol_menu, 'callback_data' => $payload_menu]);
+            $buttons[] = new InlineKeyboardButton(['text' => $symbol_menu, 'callback_data' => $payload_menu]);
         }
 
         if (!is_null($payload_right)) {
-            $buttons[] = new InlineKeyboardButton(['text' =>  '▷', 'callback_data' => $payload_right]);
+            $buttons[] = new InlineKeyboardButton(['text' => '▷', 'callback_data' => $payload_right]);
         }
         // call Grandpa's constructor
         parent::__construct(['inline_keyboard' => [$buttons]]);
